b12841d6d943b558e2bbeddfda6fb70cfb2a6a6c
Logan VanHala <thearcadecabinetmaniac@gmail.com>
2026-01-30 13:46:47 -0500

    WIP robot and platforms
    Added a bit of code for the robot which I'm still working on fixing it's movement, and added blocks with box colliders

---
+++ Assets/RobotCommandsv1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotCommandsv1 : MonoBehaviour {
+
+    /* This is the basic controller inside of the robot character itself
+	 * currently i'm implemting his basic controls, then we will test the 
+	 * controls with changable ints, then we'll move on to the proper controls
+	 * in the ui
+	 */
+    public float movespeed = 1f;
+    public static bool time;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{ 
+		//move left or right
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+            transform.position += new Vector3(movespeed, 0 * Time.deltaTime);
+        }
+
+		//Attack
+
+
+		//Jump
+
+
+		//Crouch (maybe)
+	}
+}
